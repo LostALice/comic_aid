edf8fdde1f6ecccbc7b364e523cb5d8ec9223e44
LostALice <48596994+LostALice@users.noreply.github.com>
2021-12-31 02:03:18 +0800

    Update show_code.js

--- show_code.js
+++ show_code.js
@@ -2,7 +2,7 @@
 // @name         nhentai_aid
 // @namespace    https://github.com/LostALice
 // @version      1.0
-// @description  You no need to know how to use
+// @description  You should know how to use
 // @author       Aki.no.Alice@Tyrant-Rex
 // @match        https://nhentai.net/*
 // @match        *://nhentai.net/*
@@ -11,16 +11,12 @@
 
 (function() {
     "use strict";
-
     window.addEventListener("load", function(){
         var list_ = document.querySelectorAll("#content [class=gallery] [class=cover]")
         var place = document.querySelectorAll("#content [class=gallery] [class=cover] [class=caption]")
         for (var i in list_){
-            console.log(list_[i])
             if (list_[i]){
-                var link = String(list_[i].href)
-                link = link.replace(/[^0-9]/g, "")
-                place[i].innerHTML = link + "|" + place[i].innerHTML
+                place[i].innerHTML = String(list_[i].href).replace(/[^0-9]/g, "") + "|" + place[i].innerHTML
             }
         }
     },false)
